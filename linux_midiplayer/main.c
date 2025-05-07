42f70fa831b245b95b71d81503f41d426fd9b358
Jason Hall <jasonbrianhall@gmail.com>
2025-05-07 13:07:18 -0400

    On exit

--- linux_midiplayer/main.c
+++ linux_midiplayer/main.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <termios.h>
+#include <unistd.h>
 #include "midiplayer.h"
 
+// This declaration ensures we can access the global variable from midiplayer.c
+extern struct termios old_tio;
+
+// This function will be called when the program exits
+void restore_terminal(void) {
+    // Reset terminal to original settings
+    tcsetattr(STDIN_FILENO, TCSANOW, &old_tio);
+}
+
 int main(int argc, char* argv[]) {
+    // Register the cleanup function to ensure terminal settings are restored on exit
+    atexit(restore_terminal);
+    
     char filename[MAX_FILENAME] = {0}; // Empty filename by default
     
     printf("Linux MIDI Player with SDL2\n");
